180209c490b0e898a4482052f956ef1c584c9852
Kelvin Ho <xvalvrave@gmail.com>
2021-10-11 23:52:43 +0800

    solved a bug in search music function

--- js/searchmusic.js
+++ js/searchmusic.js
@@ -2,20 +2,26 @@ let songChoosed
 let songPlayingChecker
 
 function searchMusic() {
-    let input, filter, ul, li, a, i, txtValue
+    let input, filter, ul, li, a, i, txtValue, noInput
     input = document.getElementById("inputBox")
-    filter = input.value.toLocaleLowerCase()
+    filter = input.value.toUpperCase()
     ul = document.getElementById("songList")
     li = ul.getElementsByTagName("li")
     for (i = 0; i < li.length; i++) {
         a = li[i];
         txtValue = a.textContent || a.innerText
-        //If user enter keywords, the suggested songs will pop up
-        if (txtValue.toLocaleLowerCase().startsWith(filter) > 0 || 
-            txtValue.toLocaleLowerCase().indexOf(filter) > 0) {
+        console.log(txtValue)
+        console.log(filter)
+        console.log(txtValue.toUpperCase().indexOf(filter))
+        console.log(txtValue.toUpperCase().startsWith(filter))
+            //If user enter keywords, the suggested songs will pop up
+
+        if (filter.length == 0) {
+            li[i].style.display = "none"
+        } else if (txtValue.toUpperCase().indexOf(filter) > 0 || txtValue.toUpperCase().startsWith(filter)) {
             li[i].style.display = "block"
             ul.style.opacity = 1
-            //If not, it will be null
+                //If not, it will be null
         } else {
             li[i].style.display = "none"
         }
