a8e0affffd0060aa695ba1dcd82b8d50053a8590
chachajona <58243954+chachajona@users.noreply.github.com>
2021-10-11 22:09:38 +0700

    Update on search bar

--- js/searchmusic.js
+++ js/searchmusic.js
@@ -4,14 +4,15 @@ let songPlayingChecker
 function searchMusic() {
     let input, filter, ul, li, a, i, txtValue
     input = document.getElementById("inputBox")
-    filter = input.value.toUpperCase()
+    filter = input.value.toLocaleLowerCase()
     ul = document.getElementById("songList")
     li = ul.getElementsByTagName("li")
     for (i = 0; i < li.length; i++) {
         a = li[i];
         txtValue = a.textContent || a.innerText
         //If user enter keywords, the suggested songs will pop up
-        if (txtValue.toUpperCase().indexOf(filter) > 0) {
+        if (txtValue.toLocaleLowerCase().startsWith(filter) > 0 || 
+            txtValue.toLocaleLowerCase().indexOf(filter) > 0) {
             li[i].style.display = "block"
             ul.style.opacity = 1
             //If not, it will be null
